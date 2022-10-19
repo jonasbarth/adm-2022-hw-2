796885deb679a4f130782801da25006a9107c0a5
jonasbarth <jb901@exeter.ac.uk>
2022-10-19 17:00:56 +0200

    * add possibility to choose the time precision when querying for the most common time * expose time precision in the API

--- posts/__init__.py
+++ posts/__init__.py
@@ -1,2 +1,3 @@
-from .api import most_common_time
+from .api import most_common_times
 from .api import plot_posts_intervals
+from .api import TimePrecision
--- posts/api.py
+++ posts/api.py
@@ -1,15 +1,22 @@
 """The API of the posts package"""
 from collections import Counter
+from enum import Enum
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 from .prepare import read_post_time_data
-from .publishing import date_time_to_time
 from .publishing import find_posts_between
 
 
-def most_common_time():
+class TimePrecision(Enum):
+    """Enum for specifying the time precision."""
+    SECOND = 0
+    MINUTE = 1
+    HOUR = 2
+
+
+def most_common_times(precision : TimePrecision):
     """Returns the most common time at which users publish their posts.
 
     :arg
@@ -19,13 +26,24 @@ def most_common_time():
     the most common publishing post time found in the posts dataframe
     """
     all_times = []
+    if precision == TimePrecision.SECOND:
+        for posts in read_post_time_data('data/instagram_posts.csv', nrows=5000000, chunksize=10000):
+            all_times.extend(posts.post_time.tolist())
 
-    for posts in read_post_time_data('data/instagram_posts.csv', nrows=5000000, chunksize=10000):
-        all_times.extend(posts.post_time.tolist())
+    if precision == TimePrecision.MINUTE:
+        for posts in read_post_time_data('data/instagram_posts.csv', nrows=5000000, chunksize=10000):
+            posts.post_time = posts.post_time.values.astype('<M8[m]')
+            all_times.extend(posts.post_time.tolist())
 
-    most_common_datetime = Counter(all_times).most_common(1)
-    most_common_datetime = most_common_datetime[0][0]
-    return date_time_to_time(most_common_datetime)
+    if precision == TimePrecision.HOUR:
+        for posts in read_post_time_data('data/instagram_posts.csv', nrows=5000000, chunksize=10000):
+            posts.post_time = posts.post_time.dt.floor('h')
+            all_times.extend(posts.post_time.tolist())
+
+    most_common_datetime = dict(sorted(Counter(all_times).items()))
+    labels = list(most_common_datetime.keys())
+    values = list(most_common_datetime.values())
+    return labels, values
 
 
 def plot_posts_intervals(intervals):
