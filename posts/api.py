d559d41628fa3a5b3feec84941490a551808d562
jonasbarth <jb901@exeter.ac.uk>
2022-10-25 16:13:27 +0200

    RQ3 * simplify notebook by only querying for hours * remove now unused methods and enum * simplify methods for querying for posts in intervals

--- posts/api.py
+++ posts/api.py
@@ -1,47 +1,27 @@
 """The API of the posts package"""
 from collections import Counter
-from enum import Enum
-import pandas as pd
 
 import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
 
-from .prepare import read_post_time_data
 from .publishing import find_number_of_posts_between
 
 
-class TimePrecision(Enum):
-    """Enum for specifying the time precision."""
-    SECOND = 0
-    MINUTE = 1
-    HOUR = 2
-
-
-def most_common_times(posts : pd.DataFrame, precision : TimePrecision):
+def most_common_times(posts : pd.DataFrame):
     """Returns the most common time at which users publish their posts.
 
     :arg
     posts - a pandas dataframe containing the times at which users post.
 
     :returns
-    the most common publishing post time found in the posts dataframe
+    times, counts - a tuple containing the times normalised to 1970:01:01, and the frequency during that time.
     """
-    all_times = []
-    if precision == TimePrecision.SECOND:
-        all_times = posts.post_time.tolist()
+    count_hours = Counter(posts.cts.dt.hour)
 
-    if precision == TimePrecision.MINUTE:
-        posts.post_time = posts.post_time.values.astype('<M8[m]')
-        all_times = posts.post_time.tolist()
+    times = list(map(lambda value: pd.Timestamp(1970, 1, 1, value, 0, 0), count_hours.keys()))
+    post_frequency = list(count_hours.values())
 
-    if precision == TimePrecision.HOUR:
-        posts.post_time = posts.post_time.dt.floor('h')
-        all_times = posts.post_time.tolist()
-
-    most_common_datetime = dict(sorted(Counter(all_times).items()))
-    labels = list(most_common_datetime.keys())
-    values = list(most_common_datetime.values())
-    return labels, values
+    return times, post_frequency
 
 
 def plot_posts_intervals(posts: pd.DataFrame, intervals):
@@ -51,14 +31,13 @@ def plot_posts_intervals(posts: pd.DataFrame, intervals):
     intervals - a list of tuples where each tuple contains the start and end time (start, end).
 
     :return
-    a plot
+    a plot where the intervals are sorted in ascending order and the respective number of posts in that interval.
     """
+    intervals.sort()
     interval_labels = list(map(lambda start_end: f"{start_end[0]} - {start_end[1]}", intervals))
-    number_of_posts = np.empty(len(intervals))
+    number_of_posts = find_number_of_posts_between(posts, intervals)
 
-    for posts in read_post_time_data('data/instagram_posts.csv', chunksize=10000):
-        values = find_number_of_posts_between(posts, intervals)
-        number_of_posts = np.add(number_of_posts, values)
-
+    interval_labels.reverse()
+    number_of_posts.reverse()
     return plt.barh(interval_labels, number_of_posts)
 
